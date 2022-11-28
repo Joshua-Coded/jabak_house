6ff82ef424054e045863aa4d0eb18d1a95233f92
dev_josh <Alanajoshua535@gmail.com>
2022-11-28 13:19:14 +0100

    city.py

---
+++ models/city.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" City module for JOSHUA project"""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """City model for JOSHUA project"""
+    state_id = ""
