9da30bb2f919e599ddd844f2d1646ef87c3a4922
dev_josh <Alanajoshua535@gmail.com>
2022-11-28 13:15:52 +0100

    base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" this module defines a base class for all models in our joshua project"""
+
+import uuid
+from datetime  import datetime
+
+
+class BaseModel:
+    """A base class for all models in our joshua project"""
+    def __init__(self, *args, **kwargs):
+        """ Instantiates a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+        else:
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
+
+
+            del kwargs["__class__"]
+            self.__dict__.updated(kwargs)
+
+    def __str__(self):
+        """Returns a string representation of the instantance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+
+    def save(self):
+        """ update updated_at with current timestamp when instance changed"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """ convert instance to dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({"__class__":
+        (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+
+        return dictionary
